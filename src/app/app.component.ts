97b0fa9b535e5779c19aa2d50555896883a977c4
TS BIT <tomassag@gmail.com>
2021-09-08 11:19:36 +0300

    Last changes 0908reaproj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -23,9 +24,22 @@ template: `
 <button (click)='counter = counter + 1'>Click me!</button>
 <p> {{counter2}}</p>
 <button (click)='clickHandler()'>Click me too!</button>
+ <!-- <di></di> -->
+
+<hr>
+<!-- Dvikryptis
+<input {(ngModel)}='text'>
+<p>{{ text }}</p> -->
+
+<!-- 2 vienkrypciai
+<input (input)="userInput($event)" [value]="text">
+<p>{{ text }}</p> -->
+
+
+
 `,
 
-styles: ['p {color: green;}', ' '],
+styles: ['p {color: green;}', 'p {background-color: pink;}', 'p {align: center}'],
 
 
 })
@@ -36,11 +50,17 @@ export class AppComponent {
 
   firstNumber = 4;
   secondNumber = 5;
+  text = `Placeholder text`;
 
   counter = 0;
   counter2 = 0;
   clickHandler() {
     this.counter2++;
   }
+  mouseX = 0;
+  mouseY = 0;
+  // mouseMoveHandler($mouseEvent)
+  // this.mouseX$event
+  // this.mouse.Y$event
 
 }
